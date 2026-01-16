c3a946b9bdc4a3aeafb31505261a5ab1e05ad3fe
Tate Mioton <xaknitram@outlook.com>
2026-01-16 13:49:58 -0600

    Add glad.zig for Simpler Integration

--- build.zig
+++ build.zig
@@ -7,16 +7,20 @@ pub fn build(b: *std.Build) !void {
 
     const include_src = b.option(bool, "include_src", "Add the src/ directory as an include directory") orelse false;
 
+    const mod = b.addModule("glad", .{
+        .root_source_file = b.path("glad.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    mod.addCSourceFile(.{.file = b.path("src/gl.c"), .language = .c});
+    mod.addIncludePath(b.path("include"));
+
     const lib = b.addLibrary(.{
         .name = "glad",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+        .root_module = mod,
         .linkage = .static,
     });
-    lib.root_module.addIncludePath(b.path("include"));
 
     lib.installHeadersDirectory(b.path("include/glad"), "glad", .{});
     if (include_src) {
@@ -25,6 +29,5 @@ pub fn build(b: *std.Build) !void {
         lib.installHeadersDirectory(b.path("include/glad"), "include/glad", .{});
     }
 
-    lib.root_module.addCSourceFile(.{.file = b.path("src/gl.c"), .language = .c});
     b.installArtifact(lib);
 }
